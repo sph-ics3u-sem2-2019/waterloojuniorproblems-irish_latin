efde31b9fe687c4cdd98f0eba6f445b6a3d78e1a
jeancarlos <jeancarlos.herrera@ocsbstudent.ca>
2019-04-11 09:47:51 -0400

    Update Telemarketer.java

--- src/Telemarketer.java
+++ src/Telemarketer.java
@@ -20,6 +20,16 @@ public class Telemarketer {
 		 * 12) 1,3,3,9
 		 * 13) 1,0,2,6
 		 */
+		Scanner in=new Scanner(System.in);
+		System.out.println("Number 1");
+		int num1=in.nextInt();
+		int num2=in.nextInt();
+		int num3=in.nextInt();
+		int num4=in.nextInt();
+			if (num1==8||num1==9)
+				if(num4==8||num4==9)
+					System.out.println("ignore");
+			
 	}
 
 }
